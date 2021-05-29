be7002c498e25d77018552e473d7bfe4e962d7ec
psrandh <37591222+psrandh@users.noreply.github.com>
2021-05-28 18:05:18 -0700

    Update unopt_main.c
    A minor update, added in the struct that will hold the contents of the .wav file and the descriptions for each of its members.

--- unopt_main.c
+++ unopt_main.c
@@ -3,6 +3,57 @@
 
 typedef struct wavContents
 {
+	/*
+	Going by the WAVE Specs .pdf that SH pushed to the GitHub in creating this struct
+	The purpose of this struct is for parsing the .wav file that will be uploaded
+	The struct is split in pieces, though it could be split into three different structs
+	
+	This first piece is about the Header of the .wav file that will be uploaded
+	The Header has three components: ckID1, cksize1 and WAVEID
+	
+	ckID1 refers to the Chunk ID, which is 'RIFF', taking up 4 bytes
+	cksize1 refers to the overall size of the file and is recorded in bytes - expected to be 4-8 bytes
+	WAVEID refers to the file type header and it is a string that markes it as a 'WAVE' - it is 4 bytes - PSR, 2021-05-28	
+	*/
+	
+	unsigned char ckID1[4];
+	uint32_t cksize1;
+	unsigned char WAVEID[4];
+	
+	/*
+	This second piece is about the format chunk marker of the .wav file that will be uploaded
+	The format chunk marker has the components: ckID2, cksize2, wFormatTag, nChannels, nSamplesPerSec, nAvgBytesPerSec, nBlockAlign, wBitsPerSample
+	
+	ckID2 refers to the CHUNK ID, which is 'fmt', taking up 4 bytes
+	cksize2 refers to the overall size of the format data
+	wFormatTag refers to the format code, uint16_t is used here since it is clearly smaller
+	nChannels refers to the number of interleaved channels, also uint16_t since it is smaller
+	nSamplesPerSec refers to the sampling rate (blocks per second)
+	nAvgBytesPerSec refers to the data rate
+	nBlockAlign refers to the data block size in bytes, it should be 4 bytes
+	wBitsPerSample refers to the bits per sample (it is the nBlockAlign / nChannels) - PSR, 2021-05-28
+	
+	*/
+	
+	unsigned char ckID2[4];
+	uint32_t cksize2;
+	uint16_t wFormatTag;
+	uint16_t nChannels;
+	uint32_t nSamplesPerSec;
+	uint32_t nAvgBytesPerSec;
+	uint32_t nBlockAlign;
+	
+	/*
+	This third piece is about the data of the .wav file that will be uploaded
+	The data has the components: ckID3, cksize3
+	
+	ckID3 refers to the data ID, which is 'data', taking up 4 bytes
+	cksize3 refers to the overall size of the data
+	*/
+	
+	unsigned char ckID3[4];
+	uint32_t cksize3;
+	
 } wavContents;
 
 int main(int argc, char **argv)
